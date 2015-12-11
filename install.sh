f3df978cc334a91c41cd72799b0f70928ed63175
Quentin Le Guennec <quentin.leguennec1@gmail.com>
2015-12-11 17:16:39 +0100

    add terminal

--- install.sh
+++ install.sh
@@ -2,10 +2,29 @@ source ./env.sh
 
 # Git configuration
 git config push.default simple
-git config user.name "$NAME"
-git config user.mail "$MAIL"
+git config user.name "$UNAME"
+git config user.mail "$UMAIL"
 
 # Directories
 mkdir -p $HOME/wp
 mkdir -p $HOME/img
 mkdir -p $HOME/dwl
+mkdir -p $HOME/.bin
+
+# Fonts
+echo "Installing inconsolata"
+mkdir /tmp/inconsolata
+cd /tmp/inconsolata
+curl -O http://www.fantascienza.net/leonardo/ar/inconsolatag/inconsolata-g_font.zip
+unzip *.zip
+mkdir -p $HOME/.local/share/fonts
+cp Inconsolata-g.* $HOME/.local/share/fonts
+fc-cache
+
+# Terminal
+cd $HOME/wp
+git clone "$GITADDR/st.git"
+cd st
+make
+cp st $HOME/.bin
+make clean
