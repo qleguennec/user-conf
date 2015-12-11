ff14844587d3fad5b857188907c1ad3389ee8e36
Quentin Le Guennec <quentin.leguennec1@gmail.com>
2015-12-11 15:48:34 +0100

    add directories

--- env.sh
+++ env.sh
@@ -1,5 +1,5 @@
 export PATH=$PATH:$HOME/.bin
-export NAME="Quentin Le Guennec"
-export MAIL="quentin.leguennec1@gmail.com"
+export UNAME="Quentin Le Guennec"
+export UMAIL="quentin.leguennec1@gmail.com"
 export GITUSER="qleguennec"
 export GITADDR="http://github.com/$GITUSER"
--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
-source env.sh
+source ./env.sh
 
 # Git configuration
 git config push.default simple
 git config user.name "$NAME"
 git config user.mail "$MAIL"
+
+# Directories
+mkdir -p $HOME/wp
+mkdir -p $HOME/img
+mkdir -p $HOME/dwl
