fffdf2bea452b54baf1797e703100cce3722de7e
Quentin Le Guennec <quentin.leguennec1@gmail.com>
2015-12-11 17:57:00 +0100

    add zsh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
-source ./env.sh
+source $HOME/.user/env.sh
 
 # Git configuration
-git config push.default simple
-git config user.name "$UNAME"
-git config user.mail "$UMAIL"
+git config --global push.default simple
+git config --global user.name "$UNAME"
+git config --global user.mail "$UMAIL"
 
 # Directories
 mkdir -p $HOME/wp
@@ -12,19 +12,57 @@ mkdir -p $HOME/dwl
 mkdir -p $HOME/.bin
 
 # Fonts
-echo "Installing inconsolata"
-mkdir /tmp/inconsolata
-cd /tmp/inconsolata
-curl -O http://www.fantascienza.net/leonardo/ar/inconsolatag/inconsolata-g_font.zip
-unzip *.zip
-mkdir -p $HOME/.local/share/fonts
-cp Inconsolata-g.* $HOME/.local/share/fonts
-fc-cache
+function fonts {
+	echo "Installing inconsolata"
+	mkdir /tmp/inconsolata
+	cd /tmp/inconsolata
+	curl -O http://www.fantascienza.net/leonardo/ar/inconsolatag/inconsolata-g_font.zip
+	unzip *.zip
+	mkdir -p $HOME/.local/share/fonts
+	cp Inconsolata-g.* $HOME/.local/share/fonts
+	fc-cache
+}
 
 # Terminal
-cd $HOME/wp
-git clone "$GITADDR/st.git"
-cd st
-make
-cp st $HOME/.bin
-make clean
+function term {
+	"Installing st"
+	cd $HOME/wp
+	git clone "$GITADDR/st.git"
+	cd st
+	make
+	cp st $HOME/.bin
+	make clean
+}
+
+# Zsh
+function shell {
+	echo "Installing zsh"
+	if command -v zsh > /dev/null 2>&1; then
+		sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+		curl -O /tmp/zshrc.patch https://gist.github.com/1c003a043b5c17e94178
+		cd $HOME
+		patch -p1 .zshrc < /tmp/zshrc.patch
+	else
+		echo "zsh is not installed, aborting shell setup"
+	fi
+}
+
+case $1 in
+"fonts")
+	fonts
+	;;
+"term")
+	term
+	;;
+"shell")
+	shell
+	;;
+"")
+	fonts
+	term
+	shell
+	;;
+*)
+	echo "option $1 is unknown"
+	;;
+esac
