78915db73ed2a965770b30c05d5ccd21e2ea9a57
Quentin Le Guennec <quentin.leguennec1@gmail.com>
2015-12-11 15:42:16 +0100

    initial commit

---
+++ env.sh
@@ -0,0 +1,5 @@
+export PATH=$PATH:$HOME/.bin
+export NAME="Quentin Le Guennec"
+export MAIL="quentin.leguennec1@gmail.com"
+export GITUSER="qleguennec"
+export GITADDR="http://github.com/$GITUSER"
---
+++ install.sh
@@ -0,0 +1,6 @@
+source env.sh
+
+# Git configuration
+git config push.default simple
+git config user.name "$NAME"
+git config user.mail "$MAIL"
