eada2208db864ac28e7200f61e79cc6268c7efc0
Quentin Le Guennec <quentin.leguennec1@gmail.com>
2015-12-12 15:59:29 +0100

    add vimrc

--- env.sh
+++ env.sh
@@ -3,3 +3,6 @@ export UNAME="Quentin Le Guennec"
 export UMAIL="quentin.leguennec1@gmail.com"
 export GITUSER="qleguennec"
 export GITADDR="http://github.com/$GITUSER"
+
+# zsh options
+plugins=(git tmux stack tmuxinator)
