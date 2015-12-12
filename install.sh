5b25b4fd70901c0a5e2906870f1f6858e677eec7
Quentin Le Guennec <quentin.leguennec1@gmail.com>
2015-12-12 11:28:44 +0100

    fix shell install

--- install.sh
+++ install.sh
@@ -46,9 +46,7 @@ function shell {
 	if command -v zsh > /dev/null 2>&1; then
 		echo "Installing zsh"
 		sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-		curl https://gist.githubusercontent.com/raw/1c003a043b5c17e94178/patch > /tmp/zshrc.patch
-		cd $HOME
-		patch -p1 .zshrc < /tmp/zshrc.patch
+		curl https://gist.github.com/3ba24ff659da8ba821f5 > $HOME/.zshrc
 	else
 		echo "zsh is not installed, aborting shell setup"
 	fi
